a991d423bc4ec7b3c5e62d6661238d4d64f50874
Jens Hjalmarsson <jens.hjalmarsson@gmail.com>
2020-12-16 23:26:30 +0100

    bumping frontend dependencies to their current latest versions

--- frontend-app/rollup.config.js
+++ frontend-app/rollup.config.js
@@ -1,18 +1,21 @@
 import path from 'path';
-import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+
 import alias from '@rollup/plugin-alias';
 import json from '@rollup/plugin-json';
 import image from '@rollup/plugin-image';
+import svelte from 'rollup-plugin-svelte';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
 
 function serve() {
 	let server;
-	
+
 	function toExit() {
 		if (server) server.kill(0);
 	}
@@ -50,14 +53,14 @@ export default {
       }
     }),
 		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
-			css: css => {
-				css.write('bundle.css');
+			compilerOptions: {
+				// enable run-time checks when not in production
+				dev: !production
 			}
 		}),
+		// we'll extract any component CSS out into
+		// a separate file - better for performance
+		css({ output: 'bundle.css' }),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
